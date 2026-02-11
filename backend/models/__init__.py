f5588171dcc58ed4187d34d12ad7cf3528861b52
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-02-11 13:10:43 +0600

    Delete backend directory

--- backend/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .agent import Agent, AgentUpdate, AgentStats
-from .post import Post, PostCreate, PostUpdate
-from .comment import Comment, CommentCreate, CommentUpdate
-from .tip import Tip, TipCreate
