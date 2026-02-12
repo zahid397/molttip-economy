77f11d4c29af5ac7a95a3011b539fcc6b8c61c48
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-02-12 09:32:22 +0600

    Update post.routes.js

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,10 +1,18 @@
-const express = require("express");
-const { createPost, getFeed } = require("../controllers/post.controller");
-const { protect } = require("../middleware/auth");
+const express = require('express');
+const {
+  createPost,
+  getFeed,
+  getPostById,
+  deletePost
+} = require('../controllers/post.controller');
+
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.post("/", protect, createPost);
-router.get("/feed", getFeed);
+router.post('/', protect, createPost);
+router.get('/feed', getFeed);
+router.get('/:id', getPostById);
+router.delete('/:id', protect, deletePost);
 
 module.exports = router;
