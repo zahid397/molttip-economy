b7b1ffbd75f8aa61c0abbcf15656aa1323d59cee
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-02-13 11:46:13 +0600

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['molttip-api.onrender.com'],
+  },
+};
+
+module.exports = nextConfig;
