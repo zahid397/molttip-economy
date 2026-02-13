837f6abe3c2f518346fc73d9969d4395e3bb5ad8
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-02-13 15:18:36 +0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,20 +1,25 @@
-const app = require('./app');
-const connectDB = require('./config/db');
-const logger = require('./config/logger');
-const { port, nodeEnv } = require('./config/env');
+const app = require("./app");
+const connectDB = require("./config/db");
+const logger = require("./config/logger");
+const { port, nodeEnv } = require("./config/env");
 
-process.on('uncaughtException', (err) => {
-  logger.error('UNCAUGHT EXCEPTION! Shutting down...', err);
+process.on("uncaughtException", (err) => {
+  logger.error("UNCAUGHT EXCEPTION! Shutting down...", err);
   process.exit(1);
 });
 
-connectDB().then(() => {
-  const server = app.listen(port, () => {
-    logger.info(`🚀 MoltTip API running on port ${port} in ${nodeEnv} mode`);
-  });
+connectDB()
+  .then(() => {
+    const server = app.listen(port, () => {
+      logger.info(`🚀 MoltTip API running on port ${port} in ${nodeEnv} mode`);
+    });
 
-  process.on('unhandledRejection', (err) => {
-    logger.error('UNHANDLED REJECTION! Shutting down...', err);
-    server.close(() => process.exit(1));
+    process.on("unhandledRejection", (err) => {
+      logger.error("UNHANDLED REJECTION! Shutting down...", err);
+      server.close(() => process.exit(1));
+    });
+  })
+  .catch((err) => {
+    logger.error("DB Connection Failed!", err);
+    process.exit(1);
   });
-});
