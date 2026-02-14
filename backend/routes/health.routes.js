a538e9c408bc2ad85a777d3525ff94bf4a9e3489
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-02-14 10:43:59 +0600

    Create health.routes.js

---
+++ backend/routes/health.routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { healthCheck } = require('../controllers/health.controller');
+
+router.get('/', healthCheck);
+
+module.exports = router;
