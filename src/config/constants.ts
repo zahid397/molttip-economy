6f691092f036927255bbab59f9df2e73d3984dca
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-02-27 10:52:42 +0600

    Create constants.ts

---
+++ src/config/constants.ts
@@ -0,0 +1,4 @@
+export const TOKEN_NAME = 'MOTIP';
+export const INITIAL_BALANCE = 1000;
+export const SIMULATION_INTERVAL_MS = 5000; // 5 seconds
+export const API_DELAY_MS = 300; // simulate network latency
